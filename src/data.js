2f8269c75a8ee5369eb2542203e2b8d94d8c894c
Abhi-y2003 <226330001@gmail.com>
2023-09-19 01:34:45 +0530

    data showing error because of braces[]

---
+++ src/data.js
@@ -0,0 +1,95 @@
+const data = [
+
+    {
+        id:1,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+    {
+        id:1,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+    {
+        id:1,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+    {
+        id:1,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+    {
+        id:2,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+    {
+        id:3,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+    {
+        id:4,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+    {
+        id:5,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+    {
+        id:6,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+    {
+        id:7,
+        city: "Agra",
+        info: "Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres south-east of the national capital Delhi and 330 km west of the state capital Lucknow.",
+        image: "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcS66XSQTHwbpIQkPRLERIEWYuzjhcIRZ_DluE_WlEgWoJT7cMpMz4IX4hRL68Q31BWJcs3vQSH5AK5DVoBvlSdlVC2Cg3dM",
+        price: "35,764",
+
+    },
+
+];
+
+export default data;
